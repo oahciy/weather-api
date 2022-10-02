b99b6f1d0745024b22c4037e0ea1ff7dac1e394e
Yichao <yq2550@gmail.com>
2022-10-02 17:42:29 +0100

    user input: readline package; setInterval

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const got = require("got");
-const apiKey = require("./apiKey");
-const city = "London";
-const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+const Weather = require("./weather");
+const WeatherApi = require("./weatherApi");
+const readline = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
+const WeatherView = require("./weatherView");
 
-let weatherData = null;
+const api = new WeatherApi();
+const weather = new Weather(api);
 
-got(apiUrl).then((response) => {
-  weatherData = JSON.parse(response.body);
-  console.log(weatherData);
+const rl = readline.createInterface({ input, output });
+
+rl.question("To search the weather, enter city name: ", (answer) => {
+  setInterval(() => {
+    weather.fetch(answer);
+    setTimeout(() => {
+    const weatherview = new WeatherView(weather);
+    weatherview.displayWeather();
+  }, 2000);
+  }, 5000)
+
+  rl.close();
 });
-
-console.log("Requesting weather data");
